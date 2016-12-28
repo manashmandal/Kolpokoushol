59d55c69a2ee45903ecd681ac321e9de1eefde8c
Manash Kumar Mandal <manashmndl@gmail.com>
2016-12-28 15:10:23 +0600

    preprocessed data

--- concept_network/ConceptNetwork/app.js
+++ concept_network/ConceptNetwork/app.js
@@ -1,13 +1,13 @@
 ﻿var express = require('express');
 
-const port = 3000;
+const port = 4000;
 
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, function (req, res) {
+app.listen(port, function(req, res) {
     console.log("Magic happens @" + port);
 });
